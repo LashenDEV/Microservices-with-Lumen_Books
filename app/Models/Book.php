b4660918ad88ba717abe3a1513e1a0b8b982edc5
LashenDEV <lashenkari@gmail.com>
2023-01-10 17:00:27 +0530

    Creating and implementing function of the book microservice with Lumen

---
+++ app/Models/Book.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Auth\Authenticatable;
+use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Laravel\Lumen\Auth\Authorizable;
+
+class Book extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var string[]
+     */
+    protected $fillable = [
+        'title', 'description', 'price', 'author_id'
+    ];
+}
---
+++ database/factories/BookFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Book;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use function Symfony\Component\Translation\t;
+
+class BookFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Book::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'title' => $this->faker->sentence(3, true),
+            'description' => $this->faker->sentence(6, true),
+            'price' => $this->faker->numberBetween(25, 150),
+            'author_id' => $this->faker->numberBetween(1, 50),
+        ];
+    }
+}
